fad1c35c7aa0051bb730d84ebd09a5b792c3140d
ishrat <siddiqueishrat36@gmail.com>
2024-12-17 14:18:39 -0800

    Add files via upload

---
+++ js/html/app.js
@@ -0,0 +1,14 @@
+// Question 1
+// answer
+alert(" Hello Welcome to my website");
+// Question 2 answer
+alert("please enter a valid password");
+// Question 3 answer
+alert("welcom to js land...\n Happy coding!");
+// Question 4 answer
+alert("welcome to js land...");
+alert("Happy coding! \n prevent this page from creating additional dialog");
+// Question 5 answer
+console.log("Hello...I can run Js through my web browser's console");
+
+
